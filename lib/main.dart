69ba678294b5fa5de79446277abc4dc1d3e05c40
sj-pg <saharaljohni@gmail.com>
2019-07-07 14:45:27 +0300

    flutter

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Startup Name Generator',
+      theme:ThemeData(
+        primaryColor: Colors.white,
+      ),
       home: RandomWords(),
     );
   }
@@ -23,23 +26,72 @@ class RandomWords extends StatefulWidget {
 
 class RandomWordsState extends State<RandomWords> {
   final List<WordPair> _suggestions= <WordPair>[];
+  final Set<WordPair> _saved = Set<WordPair>();
   final TextStyle _biggerFont =const TextStyle(fontSize: 18);
+  void _pushSaved() {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        builder: (BuildContext context) {
+          final Iterable<ListTile> tiles = _saved.map(
+                (WordPair pair) {
+              return ListTile(
+                title: Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+          final List<Widget> divided = ListTile
+              .divideTiles(
+            context: context,
+            tiles: tiles,
+          )
+              .toList();
+
+          return Scaffold(         // Add 6 lines from here...
+            appBar: AppBar(
+              title: Text('Saved Suggestions'),
+            ),
+            body: ListView(children: divided),
+          );                       // ... to here.
+        },
+      ),
+    );
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold (
       appBar: AppBar(
         title: Text('Startup Name Generator'),
+          actions: <Widget>[      // Add 3 lines from here...
+      IconButton(icon: Icon(Icons.list), onPressed: _pushSaved),
+    ],
       ),
       body: _buildSuggestions(),
     );
 
   }
   Widget _buildRow(WordPair pair) {
+    final bool alreadySaved = _saved.contains(pair);
   return ListTile(
   title: Text(
   pair.asPascalCase,
   style: _biggerFont,
   ),
+    trailing: Icon(   // Add the lines from here...
+    alreadySaved ? Icons.favorite : Icons.favorite_border,
+    color: alreadySaved ? Colors.red : null,
+    ),
+    onTap: () {      // Add 9 lines from here...
+      setState(() {
+        if (alreadySaved) {
+          _saved.remove(pair);
+        } else {
+          _saved.add(pair);
+        }
+      });
+    },
   );
   }
   Widget _buildSuggestions() {
